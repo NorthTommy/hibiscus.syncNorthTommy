8e79f07b1f82dbb143997aa09b510bbe67128cf7
Thomas Franke <thomas-carsten.franke@gmx.de>
2025-07-29 19:04:37 +0200

    prep repository data and initial release

--- src/de/northtommy/hibiscus/syncNorthTommy/Version.java
+++ src/de/northtommy/hibiscus/syncNorthTommy/Version.java
@@ -2,6 +2,6 @@ package de.northtommy.hibiscus.syncNorthTommy;
 
 public class Version 
 {
-	public final static String BUILD = "c86116d";
-	public final static String VERSION = "0.1";
+	public final static String BUILD = "a714f4d";
+	public final static String VERSION = "0.1.0";
 }
