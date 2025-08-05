8c06bb4d743ea21b7ca620800af61c569f1a91be
Thomas Franke <thomas-carsten.franke@gmx.de>
2025-08-05 20:03:11 +0200

    prep 0.2.0

--- src/de/northtommy/hibiscus/syncNorthTommy/Version.java
+++ src/de/northtommy/hibiscus/syncNorthTommy/Version.java
@@ -2,6 +2,6 @@ package de.northtommy.hibiscus.syncNorthTommy;
 
 public class Version 
 {
-	public final static String BUILD = "c916f0d";
-	public final static String VERSION = "0.1.1";
+	public final static String BUILD = "7bdad06";
+	public final static String VERSION = "0.2.0";
 }
